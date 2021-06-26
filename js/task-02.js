50a7d8cc4c5db805613ae199af27b78768b98082
Andrii Lypovetskyi <li_andr@ukr.net>
2021-06-26 18:33:57 +0300

    tasks01-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,35 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+// -------- Так делать нельзя !!!------------
+// for (const ingredient of ingredients) {
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('ingredients__item');
+//   itemEl.textContent = ingredient;
+  
+// ingredientsEl.append(itemEl);
+// };
+//--------- Через map ----------------------------------
+// const elementsItem = ingredients.map(ingredient => {
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('ingredients__item');
+//   itemEl.textContent = ingredient;
+//   return itemEl;
+// });
+// console.log(elementsItem);
+// ingredientsEl.append(...elementsItem);
+//---------- Через функцию ------------------------------
+const makeElementsItem = options => {
+  return options.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('ingredients__item');
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+  
+};
+const elementsItem = makeElementsItem(ingredients);
+ingredientsEl.append(...elementsItem);
