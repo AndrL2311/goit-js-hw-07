0c34b08010b746a9db3bd92f17eb821b0346935c
Andrii Lypovetskyi <li_andr@ukr.net>
2021-06-29 11:17:15 +0300

    Task04

--- js/task-04.js
+++ js/task-04.js
@@ -1,28 +1,16 @@
-const counterEl = document.querySelector('#counter');
- let valueEl = Number(counterEl.children[1].textContent);
-// targetBtn.addEventListener('click', onTargetBtnClick);
-// console.log(targetBtn);
-  
-// function onTargetBtnClick(event) {
+const counterEl = document.querySelector("#counter");
+let counterValue = Number(counterEl.children[1].textContent);
 
+// console.log(typeof counterEl.children[1].textContent); // string
+const decrement = () => {
+  counterValue -= 1;
+  counterEl.children[1].textContent = counterValue;
+};
 
-//     console.log('Клик по кнопке');
-//     console.log(event.target.dataset.action);
-//      if (event.target.dataset.action === 'decrement') { // если есть атрибут...
-//       console.log('+1');
-//     }
-// }
-let value = 0;
-console.log(counterEl.children);
-console.log(valueEl);
-// valueEl = 'qwqewe';
-// counterEl.children[1].textContent = ;
-// counterEl.chidren[0].addEventListener('click', onTargetBtnDecrementClick);
-counterEl.children[0].addEventListener('click', (event) => {
-    value -= 1;
-console.log(value);
-counterEl.children[1].textContent = value;
+const increment = () => {
+  counterValue += 1;
+  counterEl.children[1].textContent = counterValue;
+};
 
-});
-counterEl.children[2].addEventListener('click', (event) => console.log(+1));
-// counterEl.children[1].append(valueEl);
+counterEl.children[0].addEventListener("click", decrement);
+counterEl.children[2].addEventListener("click", increment);
