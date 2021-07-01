ebe154e5bf3da85b049ce3786d57615f109d081a
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-01 22:45:10 +0300

    Task05-06

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,17 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("span#name-output");
 
-function onInput() {
-    if (inputEl.value !== "") outputEl.textContent = inputEl.value
+// function onInput() {
+//     if (inputEl.value !== "") outputEl.textContent = inputEl.value
+//     else outputEl.textContent = "незнакомец";
+// }
+
+// inputEl.addEventListener("input", onInput);
+
+
+function onInput(event) {
+    // console.log(event);
+    if (event.currentTarget.value !== "") outputEl.textContent = event.currentTarget.value
     else outputEl.textContent = "незнакомец";
 }
 
