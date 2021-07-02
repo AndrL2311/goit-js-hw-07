8986957d746de2d8f0a7b78951635c826e0b5de5
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-02 20:48:36 +0300

    Tasks8

---
+++ js/task-08.js
@@ -0,0 +1,31 @@
+const refs = {
+    input: document.querySelector('#controls').children[0],
+    btnRender: document.querySelector('#controls').children[1],
+    btnDestroy: document.querySelector('#controls').children[2],
+    boxes: document.querySelector('#boxes'),
+};
+
+
+refs.btnRender.addEventListener("click", createBoxes);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
+
+
+const rndColor = () => "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+
+// console.log(rndColor());
+
+
+function createBoxes() {
+   const boxes = []; 
+   const amount = refs.input.value;
+  for (let i = 0; i < amount; i += 1) {
+    const box = `<div style="width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: ${rndColor()};"></div>`;
+    boxes.push(box);
+  }
+   return refs.boxes.insertAdjacentHTML('beforeend', boxes.join(''));
+}
+
+function destroyBoxes() {
+refs.boxes.innerHTML = "";
+refs.input.value = ""; 
+};
