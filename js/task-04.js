d3cf352f8bb30e69b89368f9c62c18842121e112
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-03 22:51:05 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,17 @@
 const counterEl = document.querySelector("#counter");
-let counterValue = Number(counterEl.children[1].textContent);
+const valueEl = document.querySelector("#value");
 
+let counterValue = Number(valueEl.textContent);
+// console.dir(counterEl.children[1]);
 // console.log(typeof counterEl.children[1].textContent); // string
 const decrement = () => {
   counterValue -= 1;
-  counterEl.children[1].textContent = counterValue;
+  valueEl.textContent = counterValue;
 };
 
 const increment = () => {
   counterValue += 1;
-  counterEl.children[1].textContent = counterValue;
+  valueEl.textContent = counterValue;
 };
 
 counterEl.children[0].addEventListener("click", decrement);
