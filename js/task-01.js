7a46f24f970d245a4d8e18adba5f92db4b26b81a
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-03 12:04:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,21 @@ const categoriesEl = document.querySelector('#categories').querySelectorAll('.it
 
 console.log(`В списке ` + categoriesEl.length + ` категории.`);
 
-for (const category of categoriesEl) {
-    // console.log(`Категория: ` + category.children[0].textContent);
-    // console.log(`Количество элементов: ` + category.children[1].children.length);
+// Через цикл for () of
+
+// for (const category of categoriesEl) {
     
+//     // console.log(`Категория: ` + category.children[0].textContent);
+//     // console.log(`Количество элементов: ` + category.children[1].children.length);
+    
+//     console.log(`Категория: ` + category.firstElementChild.textContent);
+//     console.log(`Количество элементов: ` + category.lastElementChild.children.length);
+
+// };
+
+// Через array.forEach()
+
+categoriesEl.forEach(category => {
     console.log(`Категория: ` + category.firstElementChild.textContent);
     console.log(`Количество элементов: ` + category.lastElementChild.children.length);
-
-};
+});
