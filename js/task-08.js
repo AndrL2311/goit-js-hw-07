f02d2144a2e22d18623c60b24b0fccbfaf3c2419
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-03 15:05:51 +0300

    Update task-08.js
    Переделал все функции на стрелки и разобрался как передавать аргумент в функцию  обработки события

--- js/task-08.js
+++ js/task-08.js
@@ -5,19 +5,11 @@ const refs = {
     boxes: document.querySelector('#boxes'),
 };
 
-
-refs.btnRender.addEventListener("click", createBoxes);
-refs.btnDestroy.addEventListener("click", destroyBoxes);
-
-
 const rndColor = () => "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
 
-// console.log(rndColor());
-
-
-function createBoxes() {
+const createBoxes = (amount) => {
    const boxes = []; 
-   const amount = refs.input.value;
+  // console.log(amount);
   for (let i = 0; i < amount; i += 1) {
     const box = `<div style="width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: ${rndColor()}; margin-bottom: 5px;"></div>`;
     boxes.push(box);
@@ -25,7 +17,12 @@ function createBoxes() {
    return refs.boxes.insertAdjacentHTML('beforeend', boxes.join(''));
 }
 
-function destroyBoxes() {
+const destroyBoxes = () => {
 refs.boxes.innerHTML = "";
 refs.input.value = ""; 
 };
+
+
+refs.btnRender.addEventListener("click", event => createBoxes(refs.input.value));
+refs.btnDestroy.addEventListener("click", destroyBoxes);
+
